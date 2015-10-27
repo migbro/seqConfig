25d11f2febc1ab4fed6c765e7d86a199481727d4
Dominic Fitzgerald <dominicfitzgerald11@gmail.com>
2015-10-27 16:33:49 -0500

    Started adding form for config_submit

--- forms.py
+++ forms.py
@@ -1,6 +1,13 @@
 __author__ = 'Dominic Fitzgerald'
 
 from django import forms
+from models import Config
 
 class ConfigForm(forms.ModelForm):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(ConfigForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = Config
+        fields = ['runtype', 'read1_cycles', 'read2_cycles', 'barcode_cycles',
+                  'flowcell_id', 'machine', 'created_by']
--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.context_processors import csrf
 from models import Config
+from forms import ConfigForm
 
 # Create your views here.
 def config_manage(request):
@@ -9,7 +11,16 @@ def config_manage(request):
     return render(request, 'seqConfig/config/config_manage.html', context)
 
 def config_submit(request):
-    return render(request, 'seqConfig/config/config_submit.html')
+    if request.method == 'POST':
+        config_form = ConfigForm(request.POST, instance=Config())
+        if config_form.is_valid():
+            config_form.save()
+        return HttpResponseRedirect('seqConfig/config/manage/')
+    else:
+        config_form = ConfigForm(instance=Config())
+        context = {'config_form': config_form}
+        context.update(csrf(request))
+        return render(request, 'seqConfig/config/config_submit.html', context)
 
 def config_get(request, config_id):
     config = Config.objects.get(pk=config_id)
