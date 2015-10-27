5edb06339854f643474a867237b2c6f7118cbf7e
migbro <miguel.a.brown@gmail.com>
2015-10-27 13:41:50 -0500

    Started models

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
+class Barcode(models.Model):
+    name = models.CharField(max_length=64, verbose_name='Barcode name')
+    sequence = models.CharField(max_length=64, verbose_name='Barcode sequence')
+
+    def __str__(self):
+        return '{}:{}'.format(self.name, self.sequence)
