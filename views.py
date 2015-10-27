282f22cd49d2823a016e4b40f785dfae775ce97c
Dominic Fitzgerald <dominicfitzgerald11@gmail.com>
2015-10-27 15:37:16 -0500

    Creating manage page

--- views.py
+++ views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from models import Config
 
 # Create your views here.
 def config_manage(request):
-    return render(request, 'seqConfig/config/config_manage.html')
+    configs = Config.objects.all()
+    context = {'configs': configs}
+    return render(request, 'seqConfig/config/config_manage.html', context)
 
 def config_submit(request):
     return render(request, 'seqConfig/config/config_submit.html')
