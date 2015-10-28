45a95b687fc4a8ba8225e08f681f1eee643ab12a
migbro <miguel.a.brown@gmail.com>
2015-10-28 14:27:00 -0500

    Minor code cleanup

--- views.py
+++ views.py
@@ -6,11 +6,13 @@ from models import Library
 from forms import ConfigForm
 from django.core import serializers
 
+
 # Create your views here.
 def config_manage(request):
     configs = Config.objects.all()
     context = {'configs': configs}
     return render(request, 'seqConfig/config/config_manage.html', context)
+
 
 def config_submit(request):
     if request.method == 'POST':
@@ -26,14 +28,11 @@ def config_submit(request):
 
 
 def config_get(request, flowcell_id):
-    #config = Config.objects.get(flowcell_id__iexact=flowcell_id)
     config = Config.objects.select_related().get(flowcell_id__iexact=flowcell_id)
     lanes = config.lane_set.all()
     object_list = [lane for lane in lanes]
     object_list.append(config)
     for lane in lanes:
-        print lane
         object_list.append(Library.objects.get(lane=lane.number))
     json_response = serializers.serialize('json', object_list)
-    #json_response = serializers.serialize('json', [config, ])
     return HttpResponse(json_response)
