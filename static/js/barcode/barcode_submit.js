64d15a100dd96754f6d9c8e3de437bf56c45efb4
migbro <miguel.a.brown@gmail.com>
2015-11-05 14:12:30 -0600

    Visual and functional improvements and streamlining

--- static/js/barcode/barcode_submit.js
+++ static/js/barcode/barcode_submit.js
@@ -2,14 +2,6 @@
  * Created by Dominic Fitzgerald on 10/29/15.
  */
 $(document).ready(function(){
-    $('#barcode-select').select2({
-        minimumResultsForSearch: Infinity
-    }).change(function())
-
-    $('#id_barcode').select2({
-        minimumResultsForSearch: Infinity
-    });
-
     $('.btn-submit').click(function(){
         $('form').submit();
     });
